6f4845145fc59d65b0fc7cadff53de56149b02ba
Denis Tkachenko <denis.t@billline.net>
2024-12-20 12:44:52 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isValidL(const string& input) {
+    int state = 0;
+    for (size_t i = 0; i < input.length(); ++i) {
+        char c = input[i];
+
+        if (state == 0) {
+            if (c == '%') {
+                state = 1;
+            } else {
+                return false;
+            }
+        }
+        else if (state == 1) {
+            if (isdigit(c)) {
+                state = 2;
+            } else {
+                return false;
+            }
+        }
+        else if (state == 2) {
+            if (isdigit(c)) {
+                continue;
+            } else if (c == '~' || c == '-' || c == '%') {
+                state = 3;
+            } else {
+                return false;
+            }
+        }
+        else if (state == 3) {
+            if (isupper(c) && c >= 'A' && c <= 'Z') {
+                state = 4;
+            } else {
+                return false;
+            }
+        }
+        else if (state == 4) {
+            if (isupper(c) && c >= 'A' && c <= 'Z') {
+                continue;
+            } else if (c == '%') {
+                state = 5;
+            } else {
+                return false;
+            }
+        }
+        else if (state == 5) {
+            return false;
+        }
+    }
+    return state == 5;
+}
+
+int main() {
+    string input;
+    cout << "Input: ";
+    cin >> input;
+
+    if (isValidL(input)) {
+        cout << "Valid" << endl;
+    } else {
+        cout << "Invalid" << endl;
+    }
+
+    return 0;
+}
